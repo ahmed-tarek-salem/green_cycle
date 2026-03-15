543deea5d6e90282caa5d61ebb64bcdb590d3944
Ahmed Tarek <ahmed.tarek.abdelmoaty@nawy.com>
2026-03-15 22:02:12 +0200

    build: ios with the new endpoints

--- lib/utilities/global/app_endpoints.dart
+++ lib/utilities/global/app_endpoints.dart
@@ -1,5 +1,5 @@
 class AppEndpoints {
-  static const baseUrl = 'https://green-cycle-juiq.onrender.com/api/v1/';
+  static const baseUrl = 'http://100.56.17.112/api/v1/';
   static const login = '${baseUrl}auth/log-in';
   static const signup = '${baseUrl}auth/sign-up';
   static const veifyOtp = '${baseUrl}auth/verify-otp';
